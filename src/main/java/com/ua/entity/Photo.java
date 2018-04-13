81a9e55825bad1e11ea6dd0bd61ef21bf0a5a41a
unknown <sbymix91@gmail.com>
2018-04-13 18:38:19 +0300

    d

---
+++ src/main/java/com/ua/entity/Photo.java
@@ -0,0 +1,32 @@
+package com.ua.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter @NoArgsConstructor
+public class Photo extends BaseEntity{
+
+	private String file;
+	private String name;
+	@ManyToOne
+	private Person person;
+	@ManyToOne
+	private Spilnoty spilnoty;
+	@ManyToMany(mappedBy="photoLikes",cascade = CascadeType.ALL)
+	List<Person> persons = new ArrayList<Person>();
+	@ManyToOne
+	private PhotoAlbum photoAlbum;
+	@OneToMany(mappedBy="photo",cascade = CascadeType.ALL)
+	List<PhotoComent> photoComent = new ArrayList<PhotoComent>();
+}
---
+++ src/main/java/com/ua/entity/Wall.java
@@ -0,0 +1,33 @@
+package com.ua.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter @NoArgsConstructor
+public class Wall extends BaseEntity{
+
+	private String message;
+	@ManyToOne
+	private Person fromPerson;
+	@ManyToOne
+	private Spilnoty spilnoty;
+	@ManyToOne
+	private Person person;
+	@OneToMany(mappedBy="wall",cascade = CascadeType.ALL)
+	List<WallComents> wallComents = new ArrayList<WallComents>();
+
+	
+
+	
+
+}
